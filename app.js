7b4af27405b87c8ff46b9f8119d16c1f0fdbb0f1
Sydney Mae Pagalan <smbfpagalan@gmail.com>
2023-06-29 15:15:53 -0700

    added js code

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ function warning() {
     confirm('You sure about this?!');
 }
 
+function catBreed() {
+    let cat = prompt('Give me a cat breed');
+    while(!cat) {
+        cat = prompt('Need more CAAAAATS')
+    } 
+    alert('Yay! Welcome to my cat page!');
+    
+}
+
 function catQuestion() {
-    let cat = prompt('DO YOU LIKE CATS?');         
-        
-    if (cat === 'yes') {
+    let cat = prompt('DO YOU LIKE CATS Answer Y/N');  
+    
+    if (cat === 'Y') {
         document.write ("<p>AWESOME! Then you are welcome here!</p>")
-    } else if (cat === 'no') {
+    } else if (cat === 'N') {
         document.write ('<p>GO AWAY!</p>')
     } else {
         document.write ('<p>THAT IS UNFORTUNATE BUT I FORGIVE YOU</p>')
     }
 }
+
